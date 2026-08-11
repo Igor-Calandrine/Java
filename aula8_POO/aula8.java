ec74c75edcdc8010a7324dd3e2d13613470621e0
Igor Calandrine <igorwow@hotmail.com>
2026-08-10 21:55:54 -0300

    Classe Objeto Atributos

---
+++ aula8_POO/aula8.java
@@ -0,0 +1,49 @@
+package aula8_POO;
+
+public class aula8 {
+public static void main(String[] args) {
+/*
+-Criando Classe
+
+public class NomeDaClasse {
+  *Criando Atributos
+   String a;
+   int b;
+   double c;
+
+   public int nomeDoMetodo1 (int a, int b) {
+      return a + b;
+   }
+
+   public int nomeDoMetodo2 (int a, int b) {
+      return a * b;
+   }
+   
+   public void nomeDoMetodo3 (int a) {
+   *this referencia o próprio atributo
+   this.a = a + a;
+   }
+
+}
+
+-Usando Classe 
+   No arquivo Main.java
+
+   para chamar o método em outro arquivo:
+   package NomeDaClasse;
+   
+   NomeDaClasse variavel = new NomeDaClasse();
+
+  *Para dar valor a um atributo usa-se o ponto
+   variavel.a = "Igor"
+  
+  *Para chamar um método usa-se o ponto
+   variavel.nomeDoMetodo2()
+
+*/ 
+
+
+
+
+}
+}
