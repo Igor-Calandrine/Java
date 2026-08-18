c78aa31b41f8c7ff36e3969bbfce8a97f4014cde
Igor Calandrine <igorwow@hotmail.com>
2026-08-18 01:12:04 -0300

    ArrayList

---
+++ aula10_Vetores/aula10P2.java
@@ -0,0 +1,38 @@
+package aula10_Vetores;
+
+public class aula10P2 {
+public static void main(String[] args) {
+/*
+-ArrayList
+   O ArrayList é uma estrutura de dados que funciona como um array de tamanho dinâmico.
+   Não aceita tipos primitivos diretamente, trabalha com objetos
+   Os elementos permanecem na ordem em que foram adicionados.
+   Pode percorrer com For-Each
+
+   Principais métodos:
+      add(), add(index, elemento)
+      get(index)
+      set(index, elemento)
+      remove(objeto), numeros.remove(Integer.valueOf(valor_variavel));
+      size()
+      sort()
+      isEmpty()
+      contains(elemento)
+      indexOf(elemento)
+      lastIndexOf(elemento)
+      clear()
+      removeIf()
+      toArray()
+
+   Sua sintaxe é:
+
+*     import java.util.ArrayList;
+
+*     ArrayList<TipoWrapper> nome_variavel = new ArrayList<>();
+
+-Collections
+
+
+*/ 
+}   
+}
