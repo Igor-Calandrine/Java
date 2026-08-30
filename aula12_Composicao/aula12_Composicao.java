ff13e9d4e3e7f93bab45db5439ed103f6012cf83
Igor Calandrine <igorwow@hotmail.com>
2026-08-29 23:28:08 -0300

    Enumeração

---
+++ aula12_Composicao/aula12_Composicao.java
@@ -0,0 +1,37 @@
+package aula12_Composicao;
+
+public class aula12_Composicao {
+public static void main(String[] args) {
+/*
+-Enumerações
+   Uma enumeração, representada pela palavra-chave enum em Java, é um tipo especial usado para representar um conjunto fixo e limitado de valores. Em vez de utilizar String ou números para representar informações que possuem opções pré-definidas, podemos criar um enum com os valores permitidos. Por exemplo:
+
+*   enum StatusPedido { 
+*      PENDENTE, 
+*      PROCESSANDO, 
+*      ENVIADO, 
+*      ENTREGUE, 
+*      CANCELADO 
+*   }
+
+   Dessa forma, podemos declarar uma variável do tipo StatusPedido e atribuir somente um dos valores definidos:
+
+   Sem enum:
+   StatusPedido status = "ENVIADO";
+
+   Com enum:
+   StatusPedido status = StatusPedido.ENVIADO;
+
+   Outra característica importante é que um enum não é apenas uma lista de constantes. Em Java, ele é um tipo especial que pode possuir atributos, construtores e métodos, permitindo associar dados e comportamentos aos seus valores. Isso faz com que as enumerações também possam participar da lógica do sistema, mantendo informações relacionadas ao próprio tipo de maneira organizada.
+
+
+
+
+
+
+
+*/ 
+
+
+}
+}
